af1952eba9da844eca6678954e177634d0182c4e
gatsbygg <binqhan@gmail.com>
2020-03-29 21:21:15 +0800

    Added robots-txt, sitemap, feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
 module.exports = {
     pathPrefix: '/',
     siteMetadata: require('./site-metadata.json'),
+    siteUrl: 'https://gatsbygg.netlify.com/'
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
+        'gatsby-plugin-robots-txt',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-feed'
         {
             resolve: `gatsby-source-filesystem`,
             options: {
