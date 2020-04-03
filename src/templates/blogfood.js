b0b648e48b31f44dba24ee8897410336c5403941
gatsbygg <59181154+gatsbygg@users.noreply.github.com>
2020-04-03 23:38:16 +0800

    Update blogfood.js

--- src/templates/blogfood.js
+++ src/templates/blogfood.js
@@ -7,7 +7,7 @@ import {getPages, Link, safePrefix} from '../utils';
 
 export default class Blog extends React.Component {
     render() {
-        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/postsfood'), 'frontmatter.date', 'desc');
+        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/food'), 'frontmatter.date', 'desc');
         return (
             <Layout {...this.props}>
               <div className="post-feed">
